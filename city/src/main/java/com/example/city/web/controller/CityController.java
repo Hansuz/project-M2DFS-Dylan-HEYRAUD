89d8511d461ca9469df562bf15cb827edee31c68
Dylan <heyraud.dylan@gmail.com>
2021-11-19 12:33:09 +0100

    Create CityController

--- city/src/main/java/com/example/city/web/controller/CityController.java
+++ city/src/main/java/com/example/city/web/controller/CityController.java
@@ -1,33 +1,24 @@
-package com.example.weather.web.controller;
+package com.example.city.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class WeatherController {
-
+public class CityController {
     @Autowired
     RestTemplate restTemplate;
 
-    @ApiOperation(value = "Get details for weather", response = WeatherController.class, tags = "getWeather")
-    @RequestMapping(value = "/Weather/{key}", method = RequestMethod.GET)
-    public String getWeather(@PathVariable String key) {
-        String response = restTemplate.exchange("http://dataservice.accuweather.com/currentconditions/v1/{key}?apikey=1S8eDxJkj8iHWAaf3XbuMAhGQMgQOEpS&language=fr-fr",
-                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, key).getBody();
-        return response;
-    }
-
     String city;
 
-    @ApiOperation(value = "Get details for city", response = WeatherController.class, tags = "getCity")
+    @ApiOperation(value = "Get details for city", response = CityController.class, tags = "getCity")
     @RequestMapping(value = "/City/{city}", method = RequestMethod.GET)
     public String getCity(@PathVariable String city) {
         String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=1S8eDxJkj8iHWAaf3XbuMAhGQMgQOEpS&q=" + city + "&language=fr-fr",
