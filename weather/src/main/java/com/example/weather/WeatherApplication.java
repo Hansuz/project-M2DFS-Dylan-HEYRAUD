9c0c5495711ba268bfd3463df9c85785dd906869
Dylan <heyraud.dylan@gmail.com>
2021-11-19 19:37:10 +0100

    Test Eureka

--- weather/src/main/java/com/example/weather/WeatherApplication.java
+++ weather/src/main/java/com/example/weather/WeatherApplication.java
@@ -2,7 +2,11 @@ package com.example.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
+//@EnableConfigurationProperties
 @SpringBootApplication
 public class WeatherApplication {
 
