52a5a7166cd44b259d49146f7aab193224ece853
Dylan <heyraud.dylan@gmail.com>
2021-11-19 15:30:43 +0100

    Use Swagger2 for city

--- city/src/main/java/com/example/city/web/controller/CityController.java
+++ city/src/main/java/com/example/city/web/controller/CityController.java
@@ -1,6 +1,9 @@
 package com.example.city.web.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Api(value="City controller")
+
 @RestController
 public class CityController {
     @Autowired
@@ -19,6 +24,13 @@ public class CityController {
     String city;
 
     @ApiOperation(value = "Get details for city", response = CityController.class, tags = "getCity")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @RequestMapping(value = "/City/{city}", method = RequestMethod.GET)
     public String getCity(@PathVariable String city) {
         String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=1S8eDxJkj8iHWAaf3XbuMAhGQMgQOEpS&q=" + city + "&language=fr-fr",
